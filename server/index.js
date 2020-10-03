c0a1e2dd8cbdcec5b14cc4f75518779994134086
Lars Nyström <nystrom.lasse@gmail.com>
2020-10-04 00:55:13 +0200

    Add data endpoint

--- server/index.js
+++ server/index.js
@@ -1,10 +1,27 @@
 import express from 'express';
 
+const data = {
+    questions: [{
+        questionId: "Q1",
+        content: "Question 1"
+    }],
+    answers: [{
+        answerId: "A1",
+        questionId: "Q1",
+        upvotes: 2,
+        content: "Answer 1"
+    }]
+}
+
 const app = new express();
 
 app.get('/', async (_req, res) => {
     res.send('Hello world!');
 });
+
+app.get('/data', async (_req, res) => {
+    res.json(data);
+})
 
 app.listen(7778);
 
