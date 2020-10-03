877612e20b71203872ea1f3ef366424d2c6a237e
Lars Nyström <nystrom.lasse@gmail.com>
2020-10-04 01:05:51 +0200

    Server side render something small to start with

--- server/index.js
+++ server/index.js
@@ -1,29 +1,55 @@
-import {} from 'vue';
-import express from 'express';
+import Vue from "vue";
+import { createRenderer } from "vue-server-renderer";
+import express from "express";
 
 const data = {
-    questions: [{
-        questionId: "Q1",
-        content: "Question 1"
-    }],
-    answers: [{
-        answerId: "A1",
-        questionId: "Q1",
-        upvotes: 2,
-        content: "Answer 1"
-    }]
-}
+  questions: [
+    {
+      questionId: "Q1",
+      content: "Question 1",
+    },
+  ],
+  answers: [
+    {
+      answerId: "A1",
+      questionId: "Q1",
+      upvotes: 2,
+      content: "Answer 1",
+    },
+  ],
+};
 
+const renderer = createRenderer();
 const server = new express();
 
-server.get('/', async (_req, res) => {
-    res.send('Hello world!');
+server.get("/data", async (_req, res) => {
+  res.json(data);
 });
 
-server.get('/data', async (_req, res) => {
-    res.json(data);
-})
+server.get('*', async (_req, res) => {
+  const app = new Vue({
+    data: {
+      url: _req.url,
+    },
+    template: `<div>The visited url is {{url}}</div>`,
+  });
+
+  renderer.renderToString(app, (err, html) => {
+    if (err) {
+      res.status(500).end("Internal Server Error");
+      return;
+    }
+    res.end(`
+            <!DOCTYPE html>
+            <html lang="en">
+              <head><title>Hello</title></head>
+              <body>${html}</body>
+            </html>
+          `);
+  });
+});
+
 
 server.listen(7778);
 
-console.info('Server is listening.');
+console.info("Server is listening.");
